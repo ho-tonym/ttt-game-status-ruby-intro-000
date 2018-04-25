27617d9110930436fe6dcfea8c6c252434c56d59
Tony Ho <ho.tonym@gmail.com>
2018-04-25 20:43:28 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -81,10 +81,11 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board)== "X"
+  if my_won?(board)== "X"
     return "X"
   elsif won?(board) == "O"
     return "O"
   else
     return nil
+  end
 end
