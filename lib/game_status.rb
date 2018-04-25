56e7077aa749f88bdc9ec56986900e844aaf9fb4
Tony Ho <ho.tonym@gmail.com>
2018-04-25 20:45:50 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,10 +30,8 @@ def won?(board)
 
       if board_position_1 =="X" && board_position_2 == "X" && board_position_3 =="X"
 
-        return win_index, "X"
-      elsif board_position_1 =="O" && board_position_2 == "O" && board_position_3 =="O"
+        return win_index
 
-        return win_index, "O"
       end
   end
   return false
